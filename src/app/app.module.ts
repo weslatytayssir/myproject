3285a991c048d005b4c490588a682dbf13ef0b5c
weslatytayssir <wesletytayssir@gmail.com>
2023-12-01 01:15:27 +0100

    Page d'acceuil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './accueil/header/header.component';
+import { FooterComponent } from './accueil/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfilComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
